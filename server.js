b6fbbe114849977a18256a9fdee2ab5081d15f47
federicoig <federicoignacio1@gmail.com>
2020-06-01 18:47:26 -0300

    add Procfile and server fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ let app = express()
 let port = process.env.PORT || 5000
 
 app.use(express.static("public"))
-app.use(express.static("./node_modules/bootstrap/dist/css/bootstrap.min.css"))
 
 app.listen(port, function () {
     console.log("running server")
